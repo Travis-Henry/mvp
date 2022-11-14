cbe73b40ce5658593f84b18fdffd9e660fa29653
Travis-Henry <travis.henry2016@gmail.com>
2022-11-14 11:21:17 -0700

    changed structure of frontend-server

---
+++ frontend-server/public/login.js
@@ -0,0 +1,36 @@
+console.log("Script is loaded");
+
+$('#loginButton').on('click', (e)=>{
+    console.log("Clicked");
+
+    fetch(`https://api-server-m3lv.onrender.com/api/posts`)
+    .then((response)=>{response.json})
+    .then((data)=>console.log(data))
+    .catch((error)=>console.log(error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let username = $('#username').val();
+    // let password = $('#password').val();
+    // console.log(username);
+    // console.log(password);
+    // fetch(`https://api-server-m3lv.onrender.com/api/login`, {
+    //     method:'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({'username': username, 'password': password})
+    // }).then((response)=>response.json())
+    // .then((data)=>console.log(data))
+    // .catch((error)=>{console.log(error)});
+});
