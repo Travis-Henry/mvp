d283808c72e5eb03907d6c6073bcb306bc3ac3d7
Travis-Henry <travis.henry2016@gmail.com>
2022-11-14 10:35:29 -0700

    added more api routes

--- api-server/migration.sql
+++ api-server/migration.sql
@@ -2,9 +2,25 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS posts;
 
 CREATE TABLE users(
-    id SERIAL,
-    username VARCHAR(100)
+	user_id SERIAL,
+  	username VARCHAR(30),
+  	password VARCHAR(30),
+  	CONSTRAINT user_key PRIMARY KEY (user_id)
 );
-CREATE TABLE posts();
 
-INSERT INTO users(username) VALUES('bob');
+
+CREATE TABLE posts(
+	post_id SERIAL,
+  	content TEXT,
+  	user_id INTEGER REFERENCES users(user_id),
+  	CONSTRAINT post_key PRIMARY KEY (post_id)
+);
+
+INSERT INTO users(username, password) VALUES('Hopper', 'pass'),
+				 ('travis','hello'),
+                 ('austin', 'secret');
+                 
+INSERT INTO posts(content, user_id) VALUES('I like yellow', 2),
+										  ('I like trains', 3),
+                                          ('I like pie', 1),
+                                          ('Bananas are awesome', 2);
--- api-server/server.js
+++ api-server/server.js
@@ -14,14 +14,72 @@ const PORT = config.port;
 app.use(express.json());
 
 
-app.get('/api/users', (req, res)=>{
-    pool.query(`SELECT * FROM users`)
+app.post('/api/login', (req, res, next)=>{
+    let username = req.body.username;
+    let password = req.body.password;
+    if(password === undefined || username === "undefined"){
+        next({status:401, message:"No password or username"});
+        return;
+    }
+    pool.query(`SELECT * FROM users WHERE username = '${username}'`)
+    .then((results)=>{
+        if(results.rows[0].password === password){
+            res.status(202).send("Logged In");
+        }else{
+            next({status:401, message:"Incorrect Password"});
+            return;
+        }
+    })
+    .catch((error)=>{
+        next({status:404, message:"User Not Found"});
+        return;
+    });
+});
+
+app.post('/api/login/new', (req, res, next)=>{
+    let username = req.body.username;
+    let password = req.body.password;
+    if(password === undefined || username === "undefined"){
+        next({status:401, message:"No password or username"});
+        return;
+    }
+
+    pool.query(`SELECT * FROM users WHERE username = '${username}'`)
+    .then((results)=>{
+        if(results.rows.length === 0){
+            pool.query(`INSERT INTO users(username, password) VALUES ('${username}','${password}')`)
+            .then(()=>{
+                res.status(201).send("New account created");
+            })
+            .catch((error)=>{
+                next({status:500, message:"Something went wrong"});
+                return;
+            });
+        }else{
+            next({status: 400, message:"Username already exists"});
+            return;
+        }
+    })
+    .catch((error)=>{
+        next({status: 400, message:"You are in the catch"});
+        return;
+    });
+});
+
+app.get('/api/posts', (req, res, next)=>{
+    pool.query(`SELECT posts.post_id, posts.content, posts.user_id, users.username FROM posts JOIN users ON posts.user_id = users.user_id;`)
     .then((results)=>{
         res.send(results.rows);
     })
     .catch((error)=>{
-        res.status(400).send("Bad Request");
+        next({status:500, message:"Server Error"});
+        return;
     });
+});
+
+
+app.use((err, req, res, next)=>{
+    res.status(err.status).send(err.message);
 });
 
 app.listen(PORT, ()=>{
