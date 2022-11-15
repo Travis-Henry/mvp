f8425aef46b99701314e87a5c6eb1e83a2cfbbed
Travis-Henry <travis.henry2016@gmail.com>
2022-11-14 19:51:42 -0700

    changed api

--- api-server/server.js
+++ api-server/server.js
@@ -26,7 +26,7 @@ app.post('/api/login', (req, res, next)=>{
     pool.query(`SELECT * FROM users WHERE username = '${username}'`)
     .then((results)=>{
         if(results.rows[0].password === password){
-            res.status(202).send("Logged In");
+            res.status(202).send(`${JSON.stringify(results.rows[0].username)}`);
         }else{
             next({status:401, message:"Incorrect Password"});
             return;
